a5c7915ae833dfd06ceec8585b3429492bc9ef57
David Fortin <dafortin@ciena.com>
2016-08-25 11:22:59 -0400

    First simple version of the blueprint

--- blueprints/ember-frost-lts/index.js
+++ blueprints/ember-frost-lts/index.js
@@ -1,23 +1,36 @@
 module.exports = {
   description: '',
   normalizeEntityName: function() {},
-
-  // locals: function(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
-
-  // afterInstall: function(options) {
-  //   // Perform extra work here.
-  // }
-
   afterInstall: function () {
+    const packages = []
+
+    const groups = this.getGroups()
+    for (let i = 0; i != groups; i++) {
+      const group = groups[i]
+      if (promptUser(group)) {
+        packages = packages.concat(group.packages)
+      }
+    }
+
     return this.addAddonsToProject({
+        packages: packages
+    })
+  },
+  getGroups () {
+    return [
+      {
+        name: 'package 1',
         packages: [
           {name: 'ember-d3', target: '0.2.0'}
         ]
+      }
+    ]
+  },
+  promptUser (group) {
+    // TODO show what will be install in the package
+    this.ui.prompt(group.name).then(function (reponse) {
+      return response.answer
     })
+    return false
   }
 }

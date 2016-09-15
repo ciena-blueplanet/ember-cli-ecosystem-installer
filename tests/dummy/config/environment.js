afa491a7325d09156fa11363282cd1116ef16074
David Fortin <dafortin@ciena.com>
2016-09-15 17:55:16 -0400

    Rename repo

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -39,7 +39,7 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
-    ENV.rootURL = '/frost-lts'
+    ENV.rootURL = '/ember-cli-ecosystem-installer'
     ENV.isDemo = true
   }
 

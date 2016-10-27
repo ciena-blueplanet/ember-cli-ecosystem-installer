475e6e8e76a3532252f8f03086b9f7eda9941975
David Fortin <dafortin@ciena.com>
2016-10-27 11:05:12 -0400

    Try ovewriting the package json file

--- blueprints/ember-cli-ecosystem-installer/index.js
+++ blueprints/ember-cli-ecosystem-installer/index.js
@@ -1,3 +1,5 @@
+var Bluebird = require('bluebird')
+var fsProm = require('fs-extra-promise').usePromise(Bluebird)
 var blueprint2 = require('../../lib/ember-cli-libs/models/blueprint2')
 
 module.exports = {
@@ -20,6 +22,15 @@ module.exports = {
     return blueprint2.addAddonsToProject2.call(this, {
       packages: [{name: 'ember-prop-types@^3.0.0'}],
       blueprintOptions: { saveExact: false, saveDev: true }
+    }).then(function () {
+      var path = options.target + '/package.json'
+      return fsProm.readJsonAsync(path, function (err, data) {
+        var pkgJson = JSON.parse(data)
+        var aPkg = { name: 'ember-prop-types', target: '^3.0.0' }
+        pkgJson.devDependencies[aPkg.name] = aPkg.target
+
+        return fsProm.outputJsonAsync(path, pkgJson)
+      })
     })
   }
 }
